c59d85fdf18df6e25886040b050d38d559be251d
formonster <1739237939@qq.com>
2022-05-21 21:23:03 +0800

    feat: 测试

--- packages/cli/dist/index.mjs
+++
@@ -1,8 +0,0 @@
-// src/index.ts
-import { Command } from "commander";
-import create from "@d/create";
-var program = new Command();
-program.command("create <app-name>").description("\u521B\u5EFA\u4E00\u4E2A\u65B0\u9879\u76EE").action((name, options) => {
-  create(name, options);
-});
-program.parse(process.argv);
